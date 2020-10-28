0bb0fd17297027831634d06102f2a7ef5e364af5
Alena Kamenská <alekame019@pslib.cz>
2020-10-28 10:52:53 +0100

    Přidejte soubory projektu.

---
+++ Jednoruky_bandita/JednorukyBandita.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jednoruky_bandita
+{
+    // enum se symboly
+    public enum Valec
+    {
+        sedmicka = 0,
+        pomeranc = 1,
+        tresne = 2,
+        svestky = 3,
+        citron = 4,
+    }
+
+    // třída JednorukyBandita
+    class JednorukyBandita
+    {
+        private int _konto;
+        private int _sazka;
+        public Valec Valec1;
+
+        public Valec Valec2;
+
+        public Valec Valec3;
+
+        public int Konto
+        {
+            get 
+            {
+                return _konto;
+            }
+            set
+            {
+                _konto = value;
+            }
+        }
+        public int Sazka
+        {
+            get
+            {
+                return _sazka;
+            }
+            set
+            {
+                _sazka = value;
+            }
+        }
+        // metoda na vyhodnoceni výhry/prohry
+        public bool Vysledky()
+        {
+            
+            return ((Valec1 == Valec2) && (Valec1 == Valec3)) ;
+
+        }
+        // metoda na odecteni z konta při prohře
+        public void Odecteni()
+        {
+            _konto -= _sazka;
+        }
+        // metoda na pricteni do konta při výhře
+        public void Pricteni()
+        {
+            _konto *= 11;
+        }
+        
+       
+
+    }
+
+}
---
+++ Jednoruky_bandita/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Security.Authentication.ExtendedProtection;
+
+namespace Jednoruky_bandita
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand1 = new Random();
+            Random rand2 = new Random();
+            Random rand3 = new Random();
+            JednorukyBandita JB = new JednorukyBandita();
+            Console.Write("Stav účtu: ");
+            JB.Konto=Int32.Parse(Console.ReadLine());
+            while (JB.Konto > 0)
+            {
+                
+                do
+                {
+                    Console.Write("Sázka: ");
+                    JB.Sazka = Int32.Parse(Console.ReadLine());
+                    
+                }
+                while (JB.Sazka > JB.Konto);
+                JB.Valec1 = (Valec)rand1.Next(5);
+                JB.Valec2 = (Valec)rand2.Next(5);
+                JB.Valec3 = (Valec)rand3.Next(5);
+                Console.WriteLine(JB.Valec1 + " " + JB.Valec2 + " " + JB.Valec3);
+                if (JB.Vysledky())
+                {
+                    JB.Pricteni();
+                    Console.WriteLine("Vyhrál jste");
+                }
+                else
+                {
+                    JB.Odecteni();
+                    Console.WriteLine("Prohrál jste");
+                }
+                Console.WriteLine("Stav účtu: " + JB.Konto);
+            }   
+
+        }
+    }
+}
